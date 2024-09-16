44466e80595f05599b13bcee2195dd2fa49eea44
Al Natour Mazen <mazen.natour.pro@gmail.com>
2024-09-16 17:28:20 +0200

    Update obj.fs
    add new color attribute to shader

--- glsl/obj.fs
+++ glsl/obj.fs
@@ -3,14 +3,14 @@ precision mediump float;
 
 varying vec4 pos3D;
 varying vec3 N;
-
+uniform vec3 uColor;
 
 
 
 // ==============================================
 void main(void)
 {
-	vec3 col = vec3(0.8,0.4,0.4) * dot(N,normalize(vec3(-pos3D))); // Lambert rendering, eye light source
+	vec3 col = uColor * dot(N,normalize(vec3(-pos3D))); // Lambert rendering, eye light source
 	gl_FragColor = vec4(col,1.0);
 }
 
