a81a02183ec9e2d037daace19f4ad77bb62078be
Al Natour Mazen <mazen.natour.pro@gmail.com>
2024-09-16 16:42:46 +0200

    Create plane.js
    add plane class

---
+++ src/objects/geometricObjects/plane.js
@@ -0,0 +1,66 @@
+class plane extends objectToDraw {
+    // --------------------------------------------
+    constructor() {
+        super('glsl/plane', -1, null);
+        this.initAll();
+    }
+
+    // --------------------------------------------
+    initAll() {
+        const size = 10.0;
+        const vertices = [
+            -size, -size, -0.12,
+            size, -size, -0.12,
+            size, size, -0.12,
+            -size, size, -0.12
+        ];
+
+        const texcoords = [
+            0.0, 0.0,
+            0.0, 1.0,
+            1.0, 1.0,
+            1.0, 0.0
+        ];
+
+        this.vBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+        this.vBuffer.itemSize = 3;
+        this.vBuffer.numItems = 4;
+
+        this.tBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.tBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texcoords), gl.STATIC_DRAW);
+        this.tBuffer.itemSize = 2;
+        this.tBuffer.numItems = 4;
+
+        loadShaders(this);
+    }
+
+
+    // --------------------------------------------
+    setShadersParams() {
+        this.setCommonShaderParams(this.shader, [
+            { attribName: "aVertexPosition", buffer: this.vBuffer, itemSize: this.vBuffer.itemSize },
+            { attribName: "aTexCoords", buffer: this.tBuffer, itemSize: this.tBuffer.itemSize }
+        ]);
+        mat4.identity(mvMatrix);
+        mat4.translate(mvMatrix, distCENTER);
+        mat4.multiply(mvMatrix, rotMatrix);
+
+        gl.uniformMatrix4fv(this.shader.pMatrixUniform, false, pMatrix);
+        gl.uniformMatrix4fv(this.shader.mvMatrixUniform, false, mvMatrix);
+    }
+
+    // --------------------------------------------
+    draw() {
+        if(this.shader && this.loaded === 4) {
+            this.setShadersParams();
+
+            gl.drawArrays(gl.TRIANGLE_FAN, 0, this.vBuffer.numItems);
+            gl.drawArrays(gl.LINE_LOOP, 0, this.vBuffer.numItems);
+        }
+    }
+
+
+}
