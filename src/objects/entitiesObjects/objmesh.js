67ce87f0bdf32f77828ae12459722e65c6791f92
Al Natour Mazen <mazen.natour.pro@gmail.com>
2024-09-16 16:42:33 +0200

    Obj
    add files and directory for objs

---
+++ src/objects/entitiesObjects/objmesh.js
@@ -0,0 +1,58 @@
+class objmesh extends objectToDraw {
+
+    // --------------------------------------------
+    constructor(objFname) {
+        super('glsl/obj', -1, null);
+        this.objName = objFname;
+        this.mesh = null;
+
+        this.loadObjFile(this);
+        loadShaders(this);
+    }
+
+    // --------------------------------------------
+    setShadersParams() {
+        this.setCommonShaderParams(this.shader, [
+            { attribName: "aVertexPosition", buffer: this.mesh.vertexBuffer, itemSize: this.mesh.vertexBuffer.itemSize },
+            { attribName: "aVertexNormal", buffer: this.mesh.normalBuffer, itemSize: this.mesh.vertexBuffer.itemSize }
+        ]);
+        this.shader.rMatrixUniform = gl.getUniformLocation(this.shader, "uRMatrix");
+        this.shader.mvMatrixUniform = gl.getUniformLocation(this.shader, "uMVMatrix");
+        this.shader.pMatrixUniform = gl.getUniformLocation(this.shader, "uPMatrix");
+    }
+
+    // --------------------------------------------
+    setMatrixUniforms() {
+        mat4.identity(mvMatrix);
+        mat4.translate(mvMatrix, distCENTER);
+        mat4.multiply(mvMatrix, rotMatrix);
+        gl.uniformMatrix4fv(this.shader.rMatrixUniform, false, rotMatrix);
+        gl.uniformMatrix4fv(this.shader.mvMatrixUniform, false, mvMatrix);
+        gl.uniformMatrix4fv(this.shader.pMatrixUniform, false, pMatrix);
+    }
+
+    // --------------------------------------------
+    draw() {
+        if(this.shader && this.loaded === 4 && this.mesh != null) {
+            this.setShadersParams();
+            this.setMatrixUniforms();
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.mesh.indexBuffer);
+            gl.drawElements(gl.TRIANGLES, this.mesh.indexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+        }
+    }
+
+    loadObjFile(OBJ3D) {
+        const xhttp = new XMLHttpRequest();
+
+        xhttp.onreadystatechange = function() {
+            if (xhttp.readyState === 4 && xhttp.status === 200) {
+                const tmpMesh = new OBJ.Mesh(xhttp.responseText);
+                OBJ.initMeshBuffers(gl, tmpMesh);
+                OBJ3D.mesh = tmpMesh;
+            }
+        }
+
+        xhttp.open("GET", OBJ3D.objName, true);
+        xhttp.send();
+    }
+}
