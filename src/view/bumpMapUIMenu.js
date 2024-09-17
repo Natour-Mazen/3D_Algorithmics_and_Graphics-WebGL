7431ceecdeb73354fb32130374016ffc211ad1ec
Al Natour Mazen <mazen.natour.pro@gmail.com>
2024-09-18 00:13:48 +0200

    Update bumpMapUIMenu.js
    add function to this file to handle BumpMapMenui

--- src/view/bumpMapUIMenu.js
+++ src/view/bumpMapUIMenu.js
@@ -1,3 +1,60 @@
 let bumpTexture = null;
 
-//bumpTexture = loadTexture(gl, 'res/textures/bumpMaps/flower.jpg');
+
+const BumpMapColorPicker = doc.getElementById('bump_map_color');
+
+
+// Loaders
+const BumpMapLoader = ['flower', 'cube', 'brickwall', 'moon'];
+
+
+function populateBumpMapSelector() {
+    const bumpMapSelector = doc.getElementById('bump_map_selector');
+    BumpMapLoader.forEach(function (bumpMapName) {
+        const option = doc.createElement('option');
+        option.value = bumpMapName;
+        option.textContent = bumpMapName;
+        bumpMapSelector.appendChild(option);
+    });
+}
+
+function loadBumpTexture(textureName) {
+    const texturePath = `res/textures/bumpMaps/${textureName}.jpg`;
+    bumpTexture = loadTexture(gl, texturePath);
+}
+
+function initBumpMapSelector() {
+    const bumpMapSelector = doc.getElementById('bump_map_selector');
+    bumpMapSelector.addEventListener('change', function () {
+        const selectedBumpMap = this.value;
+        if (selectedBumpMap !== 'None') {
+            loadBumpTexture(selectedBumpMap);
+            main_plane.setShaderName('glsl/LambertBumpMap');
+        } else {
+            bumpTexture = null;
+            main_plane.setShaderName('glsl/plane');
+        }
+    });
+}
+
+/**
+ * Initialize the color picker
+ */
+function initColorPicker() {
+    BumpMapColorPicker.addEventListener('input', function () {
+        main_plane.setColor( Color.hextoRGB(this.value).toArray());
+    });
+}
+
+
+/**
+ * Initialize UI components
+ */
+function initUIComponents() {
+    populateBumpMapSelector();
+    initBumpMapSelector();
+    initColorPicker()
+}
+
+// Initialize UI components
+initUIComponents();
