e8bf9e0d59bcadae3055da0d3d7bbd75908e7da7
Al Natour Mazen <mazen.natour.pro@gmail.com>
2024-09-18 12:01:10 +0200

    Update objectToDraw.js
    re organize objectToDraw class, add some js docs and add more methods to factorize code

--- src/objects/objectToDraw.js
+++ src/objects/objectToDraw.js
@@ -1,5 +1,16 @@
+/**
+ * Base class for drawable objects.
+ * @abstract
+ */
 class objectToDraw {
 
+    /**
+     * Constructor for objectToDraw.
+     * @param {string} shaderName - The name of the shader.
+     * @param {number} loaded - The loading state.
+     * @param {WebGLProgram} shader - The shader program.
+     * @throws {TypeError} If an instance of objectToDraw is created directly.
+     */
     constructor(shaderName, loaded, shader) {
         if (new.target === objectToDraw) {
             throw new TypeError("Cannot construct ObjectToDraw instances directly");
@@ -10,36 +21,84 @@ class objectToDraw {
         this.color = Color.BLACK;
     }
 
-    draw() {
-        throw new Error("Method 'draw()' must be implemented.");
+    /**
+     * Auxiliary method to draw the object.
+     * @abstract
+     * @throws {Error} If the method is not implemented in a subclass.
+     */
+    drawAux() {
+        throw new Error("Method 'drawAux()' must be implemented.");
     }
 
+    /**
+     * Sets the shader parameters.
+     * @abstract
+     * @throws {Error} If the method is not implemented in a subclass.
+     */
     setShadersParams() {
         throw new Error("Method 'setShadersParams()' must be implemented.");
     }
 
-    setCommonShaderParams(shader, buffers) {
-        gl.useProgram(shader);
+    /**
+     * Sets the shader uniforms.
+     * @abstract
+     * @throws {Error} If the method is not implemented in a subclass.
+     */
+    setUniforms() {
+        throw new Error("Method 'setUniforms()' must be implemented.");
+    }
+
+    /**
+     * Draws the object if the shader is loaded.
+     * this method should be called in the render loop and not be overridden except for special cases like the objmesh class.
+     */
+    draw() {
+        if(this.shader && this.loaded === 4) {
+            this.setShadersParams();
+            this.setUniforms();
+            this.drawAux();
+        }
+    }
+
+    /**
+     * Sets common shader parameters.
+     * @param {Array} buffers - The buffers to use for shader attributes.
+     */
+    setCommonShaderParams(buffers) {
+        gl.useProgram(this.shader);
 
         for (const buffer of buffers) {
-            const attribLocation = gl.getAttribLocation(shader, buffer.attribName);
+            const attribLocation = gl.getAttribLocation(this.shader, buffer.attribName);
             gl.enableVertexAttribArray(attribLocation);
             gl.bindBuffer(gl.ARRAY_BUFFER, buffer.buffer);
             gl.vertexAttribPointer(attribLocation, buffer.itemSize, gl.FLOAT, false, 0, 0);
         }
 
-        shader.pMatrixUniform = gl.getUniformLocation(shader, "uPMatrix");
-        shader.mvMatrixUniform = gl.getUniformLocation(shader, "uMVMatrix");
+        this.shader.pMatrixUniform = gl.getUniformLocation(this.shader, "uPMatrix");
+        this.shader.mvMatrixUniform = gl.getUniformLocation(this.shader, "uMVMatrix");
+        this.shader.colorUniform = gl.getUniformLocation(this.shader, "uColor");
     }
 
+    /**
+     * Sets the color of the object.
+     * @param {Array} color - The color to apply.
+     */
     setColor(color) {
         this.color = color;
     }
 
+    /**
+     * Sets the scale of the object.
+     * @param {number} scale - The scale to apply.
+     */
     setScale(scale) {
         this.scale = scale;
     }
 
+    /**
+     * Sets the shader name and reloads the shaders.
+     * @param {string} newShaderName - The new shader name.
+     */
     setShaderName(newShaderName) {
         this.shaderName = newShaderName;
         loadShaders(this);
