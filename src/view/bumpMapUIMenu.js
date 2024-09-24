2ba49134ded775fcd45d563449a0e8e081d437ea
Mazen-Serli <mazen.natour@serli.com>
2024-09-24 16:32:43 +0200

    Update bumpMapUIMenu.js
    factorize the bumpMap UI Menu

--- src/view/bumpMapUIMenu.js
+++ src/view/bumpMapUIMenu.js
@@ -2,83 +2,70 @@
 let bumpMap = null;
 let texture_ForBump = null;
 
-const BumpMapSelector = doc.getElementById('bump_map_selector');
-const BumpMapShaderSelector = doc.getElementById('bump_map_shader_selector');
-const BumpMapColorPicker = doc.getElementById('bump_map_color');
-const BumpMapTexture = doc.getElementById('bump_map_texture_selector');
+const bumpMapElements = {
+    selector: doc.getElementById('bump_map_selector'),
+    shaderSelector: doc.getElementById('bump_map_shader_selector'),
+    colorPicker: doc.getElementById('bump_map_color'),
+    textureSelector: doc.getElementById('bump_map_texture_selector')
+};
 
-const BumpMapLoader = ['brickBM.jpg', 'wavesBM.jpg', "brickNM.png", "circleNM.png", "brickNM2.png",
-    "testNM.png", "bumpWaterNM.jpg", "brickWallNM.jpg", "waterReelNM.jpg"];
-const BumpMapTextureLoader = ['brickBM.jpg', 'poolWater.png', 'seaWater.jpg', 'circle.png', "white.png", "bumpWater.jpg",
-    "brickWall.jpg", "waterReel.jpg"];
-
-const BumpMapShaderLoader = ['Lambert', 'Blinn-Phong'];
+const bumpMapLoader = ['brickBM.jpg', 'wavesBM.jpg', "brickNM.png", "circleNM.png", "brickNM2.png", "testNM.png", "bumpWaterNM.jpg", "brickWallNM.jpg", "waterReelNM.jpg"];
+const bumpMapTextureLoader = ['brickBM.jpg', 'poolWater.png', 'seaWater.jpg', 'circle.png', "white.png", "bumpWater.jpg", "brickWall.jpg", "waterReel.jpg"];
+const bumpMapShaderLoader = ['Lambert', 'Blinn-Phong'];
 
 let selectedBumpMap = "None";
 let selectedTexture = "None";
 let selectedShader = "None";
 
-function initUIComponents() {
-    initSelector(BumpMapSelector, BumpMapLoader, function () {
-        selectedBumpMap = this.value;
-        if(selectedTexture !== "None") {
-            handleShader();
-        }
-        main_plane.setColor(Color.hextoRGB(BumpMapColorPicker.value).toArray());
-    });
 
-    initSelector(BumpMapTexture, BumpMapTextureLoader, function () {
-        selectedTexture = this.value;
-        handleShader();
-        main_plane.setColor(Color.hextoRGB(BumpMapColorPicker.value).toArray());
-    });
-
-    initSelector(BumpMapShaderSelector, BumpMapShaderLoader, function () {
-        selectedShader = this.value;
-        handleShader();
-    });
-
-    initColorPicker(BumpMapColorPicker, function () {
-        main_plane.setColor(Color.hextoRGB(this.value).toArray());
-    });
-}
-
-function handleShader()
-{
-    // If wwe don't have a texture.
-    if (selectedTexture === "None")
-    {
+function handleShader() {
+    if (selectedTexture === "None") {
         bumpMap = null;
         texture_ForBump = null;
         main_plane.setShaderName('glsl/plane');
-    }
-    else // We have a texture.
-    {
-        // We load it.
+    } else {
         const texturePath = `res/textures/${selectedTexture}`;
         texture_ForBump = loadTexture(gl, texturePath);
-        // Not bumpMap, we just display the texture.
-        if(selectedBumpMap === "None" || selectedShader === "None")
-        {
+        if (selectedBumpMap === "None" || selectedShader === "None") {
             bumpMap = null;
             main_plane.setShaderName('glsl/planeTexture');
-        }
-        else // We display the bumpMap with the texture.
-        {
+        } else {
             const bumpMapPath = `res/bumpMaps/${selectedBumpMap}`;
             bumpMap = loadTexture(gl, bumpMapPath);
-            if(selectedShader === "Lambert")
-            {
+            if (selectedShader === "Lambert") {
                 main_plane.setShaderName('glsl/lambertNormalMap');
-            }else if(selectedShader === "Blinn-Phong"){
+            } else if (selectedShader === "Blinn-Phong") {
                 main_plane.setShaderName('glsl/blinnPhongNormalMap');
-            }else {
-               // pop up a message
+            } else {
                 window.alert("Please select a shader");
             }
         }
     }
 }
 
+function initUIComponents() {
+    initSelector(bumpMapElements.selector, bumpMapLoader, function () {
+        selectedBumpMap = this.value;
+        if (selectedTexture !== "None") {
+            handleShader();
+        }
+        main_plane.setColor(Color.hextoRGB(bumpMapElements.colorPicker.value).toArray());
+    });
+
+    initSelector(bumpMapElements.textureSelector, bumpMapTextureLoader, function () {
+        selectedTexture = this.value;
+        handleShader();
+        main_plane.setColor(Color.hextoRGB(bumpMapElements.colorPicker.value).toArray());
+    });
+
+    initSelector(bumpMapElements.shaderSelector, bumpMapShaderLoader, function () {
+        selectedShader = this.value;
+        handleShader();
+    });
+
+    initColorPicker(bumpMapElements.colorPicker, function () {
+        main_plane.setColor(Color.hextoRGB(this.value).toArray());
+    });
+}
 
 initUIComponents();
