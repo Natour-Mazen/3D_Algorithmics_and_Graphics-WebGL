ae31197e0cf09eeefe638ac733dd364c2f864b71
Mazen-Serli <mazen.natour@serli.com>
2024-09-24 16:27:27 +0200

    Update objectsUIMenu.js
    factorize the objects Menu Code

--- src/view/objectsUIMenu.js
+++ src/view/objectsUIMenu.js
@@ -1,57 +1,55 @@
 // src/view/objectsUIMenu.js
 
-const selects = doc.getElementsByClassName('selector');
-const planeToggle = doc.getElementById('plane_checkbox');
-const modelColorPicker = doc.getElementById('model_color');
-const scaleSlider = doc.getElementById('scale_slider');
-const scaleValueDisplay = doc.getElementById('scale_value');
+const objectsElements = {
+    selector: doc.getElementById('models_select'),
+    planeToggle: doc.getElementById('plane_checkbox'),
+    colorPicker: doc.getElementById('model_color'),
+    scaleSlider: doc.getElementById('scale_slider'),
+    scaleValueDisplay: doc.getElementById('scale_value')
+};
 
 let obj = null;
 let isTherePlane = true;
 const DEFAULT_SCALE = 8;
-
 const ObjectLoader = ['bunny', 'mustang', 'porsche', 'sphere'];
 
-function initPlaneToggle() {
-    planeToggle.checked = isTherePlane = true;
-    planeToggle.addEventListener('input', function () {
-        isTherePlane = this.checked;
-    });
-}
 
-function initObjectSelector() {
-    initSelector(selects[0], ObjectLoader, function () {
-        const selectedObject = this.value;
-        main_objectsToDraw = main_objectsToDraw.filter(obj => !(obj instanceof objmesh));
-        if (selectedObject !== 'None') {
-            let objName = 'res/obj/' + selectedObject + '.obj';
-            obj = new objmesh(objName);
-            main_objectsToDraw.push(obj);
-            obj.setColor(Color.hextoRGB(modelColorPicker.value).toArray());
-            let scaleSliderValue = scaleSlider.value;
-            if (scaleSliderValue === '0') {
-                updateScaleOBJ(DEFAULT_SCALE);
-            } else {
-                updateScaleOBJ(parseInt(scaleSliderValue));
-            }
+function handleObjectSelection(selectedObject) {
+    main_objectsToDraw = main_objectsToDraw.filter(obj => !(obj instanceof objmesh));
+    if (selectedObject !== 'None') {
+        let objName = 'res/obj/' + selectedObject + '.obj';
+        obj = new objmesh(objName);
+        main_objectsToDraw.push(obj);
+        obj.setColor(Color.hextoRGB(objectsElements.colorPicker.value).toArray());
+        let scaleSliderValue = objectsElements.scaleSlider.value;
+        if (scaleSliderValue === '0') {
+            updateScaleOBJ(DEFAULT_SCALE);
+        } else {
+            updateScaleOBJ(parseInt(scaleSliderValue));
         }
-    });
+    }
 }
 
 function updateScaleOBJ(scale) {
-    scaleValueDisplay.textContent = String(scale);
-    scaleSlider.value = scale;
+    objectsElements.scaleValueDisplay.textContent = String(scale);
+    objectsElements.scaleSlider.value = scale;
     obj.setScale(scale);
 }
 
 function initUIComponents() {
-    initPlaneToggle();
-    initColorPicker(modelColorPicker, function () {
-        obj.setColor(Color.hextoRGB(this.value).toArray());
+    initToggle(objectsElements.planeToggle, true, function () {
+        isTherePlane = this.checked;
     });
-    initObjectSelector();
-    initSlider(scaleSlider, function () {
-        if(obj !== null){
+    initColorPicker(objectsElements.colorPicker, function () {
+        if (obj !== null) {
+            obj.setColor(Color.hextoRGB(this.value).toArray());
+        }
+    });
+    initSelector(objectsElements.selector, ObjectLoader, function () {
+        handleObjectSelection(this.value);
+    });
+    initSlider(objectsElements.scaleSlider, function () {
+        if (obj !== null) {
             updateScaleOBJ(this.value);
         }
     });
