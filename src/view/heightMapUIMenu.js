1452710f594034882ea7d798a5b091c2b1600853
Al Natour Mazen <mazen.natour.pro@gmail.com>
2024-10-06 17:32:47 +0200

    Update heightMapUIMenu.js
    remove usless function and add jsdoc

--- src/view/heightMapUIMenu.js
+++ src/view/heightMapUIMenu.js
@@ -2,7 +2,6 @@ const heightMapElements = {
     toggle: doc.getElementById('heightMap_checkbox'),
     selector: doc.getElementById('heightMap_selector'),
     textureSelector: doc.getElementById('heightMap_texture_selector'),
-    //colorPicker: doc.getElementById('heightMap_color'),
     scaleSlider: doc.getElementById('heightMap_scale_slider'),
     scaleValueDisplay: doc.getElementById('heightMap_scale_value'),
     flattenSlider: doc.getElementById('heightMap_flatten_slider'),
@@ -23,12 +22,9 @@ let flattenFactorHeightMap = 1;
 const DEFAULT_HEIGHTMAP_TEXTURE = 'res/textures/white.png';
 const DEFAULT_HEIGHTMAPSCALE = 1;
 
-function initHeightMapToggle() {
-    initToggle(heightMapElements.toggle, isWireFrameActiveHeightMap = false, function () {
-        isWireFrameActiveHeightMap = this.checked;
-    });
-}
-
+/**
+ * Handles the creation of a new height map.
+ */
 function handleCreateHeightMap() {
     main_objectsToDraw = main_objectsToDraw.filter(obj => !(obj instanceof heightMap));
     theHeightMap = new heightMap();
@@ -37,6 +33,12 @@ function handleCreateHeightMap() {
     main_objectsToDraw.push(theHeightMap);
 }
 
+
+/**
+ * Handles the selection of a height map.
+ * @param {string} selectedHeightMap - The selected height map.
+ * @param {string} selectionType - The type of selection ('type' or 'texture').
+ */
 function handleHeightMapSelection(selectedHeightMap, selectionType) {
     main_objectsToDraw = main_objectsToDraw.filter(obj => !(obj instanceof heightMap));
     if (selectedHeightMap !== 'None') {
@@ -68,18 +70,10 @@ function handleHeightMapSelection(selectedHeightMap, selectionType) {
     }
 }
 
-function initHeightMapSelector() {
-    initSelector(heightMapElements.selector, heightMapLoader, function () {
-        handleHeightMapSelection(this.value, 'type');
-    });
-}
-
-function initHeightMapTexturePathMapSelector() {
-    initSelector(heightMapElements.textureSelector, heightMapTextureLoader, function () {
-        handleHeightMapSelection(this.value, 'texture');
-    });
-}
-
+/**
+ * Updates the scale of the height map.
+ * @param {number} scale - The new scale value.
+ */
 function updateScaleHeightMap(scale) {
     if (theHeightMap !== null) {
         heightMapElements.scaleValueDisplay.textContent = String(scale);
@@ -88,10 +82,11 @@ function updateScaleHeightMap(scale) {
     }
 }
 
+/**
+ * Handles the switch for the height map.
+ */
 function handleHeightMapSwitch() {
     const heightMapTextureSelectorItem = heightMapElements.textureSelector.closest('.row');
-    //const heightMapColorItem = heightMapElements.colorPicker.closest('.row');
-
 
     if (heightMapElements.switch.checked) {
         heightMapTextureSelectorItem.style.display = 'none';
@@ -107,10 +102,22 @@ function handleHeightMapSwitch() {
     }
 }
 
+/**
+ * Initializes the UI components for the height map.
+ */
 function initHeightMapUIComponents() {
-    initHeightMapToggle();
-    initHeightMapSelector();
-    initHeightMapTexturePathMapSelector();
+
+    initToggle(heightMapElements.toggle, isWireFrameActiveHeightMap = false, function () {
+        isWireFrameActiveHeightMap = this.checked;
+    });
+
+    initSelector(heightMapElements.selector, heightMapLoader, function () {
+        handleHeightMapSelection(this.value, 'type');
+    });
+
+    initSelector(heightMapElements.textureSelector, heightMapTextureLoader, function () {
+        handleHeightMapSelection(this.value, 'texture');
+    });
 
     initSlider(heightMapElements.scaleSlider, function () {
         updateScaleHeightMap(this.value);
