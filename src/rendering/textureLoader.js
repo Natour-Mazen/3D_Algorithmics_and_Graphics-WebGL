45fea109fb88e554e3398092d5c31ac2c5de67dc
Léo Hervouet <leoohervouet@gmail.com>
2024-12-29 17:49:45 +0100

    Added the possibility to remove linear interpolation
    For the images used in the shader

--- src/rendering/textureLoader.js
+++ src/rendering/textureLoader.js
@@ -19,9 +19,17 @@ function handleImageLoad(gl, texture, image, callback) {
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA8, gl.RGBA, gl.UNSIGNED_BYTE, image);
 
+    const linearTextureRead = true;
+
     gl.generateMipmap(gl.TEXTURE_2D);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    if(linearTextureRead){
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    }
+    else{
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST_MIPMAP_NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    }
 
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
